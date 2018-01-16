5ebf8f6dcfe8144586ceaaef8c9b85468c3aae09
MikKarl <mikkokk@metropolia.fi>
2018-01-16 13:17:09 +0200

    muutoksia

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,21 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { MainContentComponent } from './main-content/main-content.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavigationComponent,
+    MainContentComponent,
+    SideBarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
